3e2cc1f8bba57124019f4234478db5ad8f2316e6
Vaishali2127 <vaishalimahour8@gmail.com>
2021-09-18 17:55:26 +0530

    Navbar optimized

--- src/components/Navbar/styles/index.js
+++ src/components/Navbar/styles/index.js
@@ -1,35 +1,62 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-    height: 2rem;
+    /* height: 4rem; */
 `
 export const Card = styled.div`
     display: flex;
     justify-content: space-between;
+    align-items: center;
     margin: 0 ;
     background-color: #000;
 `
 export const Para = styled.p`
     color: #fff;
-    font-size: .7rem;
+    font-size: 1.3rem;
     margin-left: 1rem;
-    /* font-weight: 500; */
+    letter-spacing: .1rem;
+    font-weight: 800;
+
+    @media (max-width:600px){
+        font-size: 1rem;
+    }
+    @media (max-width:400px){
+        font-size: 0.7rem;
+    }
 `
-export const Button=styled.button`
-    font-size: .7rem;
-    height: 1.7rem;
-    width: 5rem;
-    margin-right: 1rem;
+export const Button = styled.button`
+    height: 2.8rem;
+    width: 8rem;
+    font-size: 1.3rem;
+    font-weight: 600;
+    margin-right: 2rem;
     color: white;
     /* background-color: purple; */
     background-color: transparent;
-    border: solid white 1px;
-    border-radius:5px;
+    /* border: solid white 1px; */
+    border: none;
+    /* border-left: 3px solid #cb0; */
+    border-bottom: 3px solid #cb0;
+    /* border-radius:10px; */
     /* opacity: .4; */
 
     &:hover{
-        background-color: purple;
-        border-radius: 15px;
+        background-color: #18c;
+        border-radius: 25px;
+        border: none;
         transition: transform 2s ease-in-out;
+    }
+    
+    @media (max-width:600px){
+        margin-right: 1rem;
+        font-size: 1rem;
+        height: 2rem;
+        width: 5rem;
+    }
+    @media (max-width:400px){
+        margin-right: 0.7rem;
+        font-size: 0.5rem;
+        height: 1.2rem;
+        width: 2rem;
     }
 `
